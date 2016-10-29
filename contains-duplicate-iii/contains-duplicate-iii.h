bddc827bb1b8244e8a989849f56eb09c12219199
Fedor Zhukov <azryve@yandex-team.ru>
2016-10-30 00:22:09 +0300

    rewrite dup

--- contains-duplicate-iii/contains-duplicate-iii.h
+++ contains-duplicate-iii/contains-duplicate-iii.h
@@ -9,23 +9,23 @@
 #ifndef __leetcode__contains_duplicate_iii__
 #define __leetcode__contains_duplicate_iii__
 
-#include <stdio.h>
-#include <cstdlib>
-#include <vector>
 #include <set>
 
 
 class Solution {
 public:
     bool containsNearbyAlmostDuplicate(const std::vector<int>& nums, int k, int t) {
-        std::set<int> window; // set is ordered automatically
-        for (int i = 0; i < nums.size(); i++) {
-            if (i > k) window.erase(nums[i-k-1]); // keep the set contains nums i j at most k
-            // |x - nums[i]| <= t  ==> -t <= x - nums[i] <= t;
-            auto pos = window.lower_bound(nums[i] - t); // x-nums[i] >= -t ==> x >= nums[i]-t
-            // x - nums[i] <= t ==> |x - nums[i]| <= t
-            if (pos != window.end() && *pos - nums[i] <= t) return true;
-            window.insert(nums[i]);
+        if (k == 0)
+            return false;
+
+        std::set<int> set;
+        for (auto it = nums.begin(); it != nums.end(); it++)
+        {
+            if (it - nums.begin() > k) set.erase(*(it-k-1));
+            auto lb = set.lower_bound(*it - t);
+            if (lb != set.end() && *lb - *it <= t)
+                return true;
+            set.insert(*it);
         }
         return false;
     }
